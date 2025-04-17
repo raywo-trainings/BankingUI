2a67b28ba5cb56503d7184b65ee539c68650a94e
Ray Wojciechowski <ray.wojciechowski@ars.de>
2025-04-17 08:58:52 +0200

    Add client editing functionality
    Introduce `ClientEditComponent` for client creation and updates via
    modal dialogs. Update `ClientListComponent` to handle add, edit, and
    delete operations, and extend `ClientService` with methods for
    managing client data.

---
+++ src/app/clients/components/client-edit/client-edit.component.ts
@@ -0,0 +1,52 @@
+import { Component, effect, inject, input } from "@angular/core";
+import { Client } from "../../models/client.model";
+import { FormsModule } from "@angular/forms";
+import { NgbActiveModal } from "@ng-bootstrap/ng-bootstrap";
+
+
+@Component({
+  selector: "app-client-edit",
+  imports: [
+    FormsModule
+  ],
+  templateUrl: "./client-edit.component.html"
+})
+export class ClientEditComponent {
+
+  private readonly modal = inject(NgbActiveModal);
+
+  protected firstname: string = "";
+  protected lastname: string = "";
+
+  public client = input.required<Client>();
+
+
+  constructor() {
+    effect(() => {
+      this.firstname = this.client().firstname;
+      this.lastname = this.client().lastname;
+    });
+  }
+
+
+  protected isEdit(): boolean {
+    return this.client().id !== undefined;
+  }
+
+
+  protected onCancel() {
+    this.modal.dismiss();
+  }
+
+
+  protected onSubmit() {
+    const newClient: Client = {
+      ...this.client(),
+      firstname: this.firstname,
+      lastname: this.lastname
+    };
+
+    this.modal.close(newClient);
+  }
+
+}
