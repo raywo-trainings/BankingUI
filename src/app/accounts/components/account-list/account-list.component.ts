d2a7026966e0d11277c7fb4a8696f31cbf116d87
Ray Wojciechowski <ray.wojciechowski@ars.de>
2025-04-23 10:55:30 +0200

    Add client-side filtering for account list
    Introduce a filter input to allow users to filter accounts by IBAN or
    client name. Update the account list display to reflect filtered
    results and sync filter state with URL query parameters.

--- src/app/accounts/components/account-list/account-list.component.ts
+++ src/app/accounts/components/account-list/account-list.component.ts
@@ -1,18 +1,21 @@
-import { Component, effect, inject, input, OnDestroy, OnInit, signal } from "@angular/core";
+import { Component, effect, inject, input, model, OnDestroy, OnInit, signal } from "@angular/core";
 import { AccountsService } from "../../services/accounts.service";
 import { AsyncPipe } from "@angular/common";
 import { AddButtonComponent } from "../../../common/components/add-button/add-button.component";
-import { RouterLink } from "@angular/router";
+import { ActivatedRoute, Router, RouterLink } from "@angular/router";
 import { AccountRowViewComponent } from "../account-row-view/account-row-view.component";
 import { CurrentAccountEditComponent } from "../current-account-edit/current-account-edit.component";
 import { NgbModal } from "@ng-bootstrap/ng-bootstrap";
 import { Account } from "../../models/account.model";
-import { Subscription } from "rxjs";
+import { combineLatest, map, Observable, of, Subscription } from "rxjs";
 import { createEmptyCurrentAccount } from "../../models/current-account.model";
 import { SavingsAccountEditComponent } from "../savings-account-edit/savings-account-edit.component";
 import { createEmptySavingsAccount } from "../../models/savings-account.model";
 import { ClientService } from "../../../clients/services/client.service";
 import { Client } from "../../../clients/models/client.model";
+import { FilterInputComponent } from "../../../common/components/filter-input/filter-input.component";
+import { toObservable } from "@angular/core/rxjs-interop";
+import { fullName } from "../../../clients/pipes/fullName.pipe";
 
 
 @Component({
@@ -21,7 +24,8 @@ import { Client } from "../../../clients/models/client.model";
     AsyncPipe,
     AddButtonComponent,
     RouterLink,
-    AccountRowViewComponent
+    AccountRowViewComponent,
+    FilterInputComponent
   ],
   templateUrl: "./account-list.component.html",
   styleUrl: "./account-list.component.scss"
@@ -31,11 +35,16 @@ export class AccountListComponent implements OnInit, OnDestroy {
   private readonly clientService = inject(ClientService);
   private readonly accountService = inject(AccountsService);
   private readonly modalService = inject(NgbModal);
+  private readonly router = inject(Router);
+  private readonly activatedRoute = inject(ActivatedRoute);
 
   private subscriptions: Subscription[] = [];
 
   protected accounts$ = this.accountService.accounts$;
   protected readonly clients$ = this.clientService.clients$;
+  protected filterText = model<string>();
+  protected filterText$ = toObservable(this.filterText);
+  protected filteredAccounts$: Observable<Account[]> = of([]);
 
   public client = input<Client>();
 
@@ -48,12 +57,46 @@ export class AccountListComponent implements OnInit, OnDestroy {
         this.accounts$ = this.accountService.getAccountsForClient(client);
       }
     });
+
+    effect(() => {
+      void this.router.navigate([], {
+        relativeTo: this.activatedRoute,
+        queryParams: {
+          filterText: this.filterText()
+        },
+        queryParamsHandling: "merge"
+      });
+    });
   }
 
 
   public ngOnInit() {
     this.accountService.updateAccounts();
     this.clientService.updateClients();
+
+    this.subscriptions.push(
+      this.activatedRoute.queryParams
+        .subscribe(params => {
+          const filterText = params["filterText"] as string | null | undefined;
+
+          if (filterText) this.filterText.set(filterText);
+        })
+    );
+
+    this.filteredAccounts$ = combineLatest([this.filterText$, this.accounts$])
+      .pipe(
+        map(([filterText, accounts]) => {
+          if (!filterText || filterText === "") return accounts;
+
+          return accounts.filter(account => {
+            const filter = filterText.toLowerCase();
+            const iban = account.iban?.toLowerCase();
+            const owner = fullName(account.owner).toLowerCase();
+
+            return iban?.includes(filter) || owner.includes(filter);
+          });
+        })
+      );
   }
 
 
